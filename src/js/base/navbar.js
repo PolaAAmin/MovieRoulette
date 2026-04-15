294d66d5986ae872e17696149722c8c745cef073
PolaAAshraf <p.ashraf2204@nu.edu.eg>
2026-04-15 18:01:16 +0200

    base navbar script

--- src/js/base/navbar.js
+++ src/js/base/navbar.js
@@ -1,11 +1,14 @@
 const accountBtn = {
-    editProfile: document.getElementById,
-    signIn: document.getElementById,
-    signUp: document.getElementById,
-    signOut: document.getElementById,
+    editProfile: document.getElementById("profile"),
+    changePassword: document.getElementById("change-password"),
+    signIn: document.getElementById("sign-in"),
+    signUp: document.getElementById("sign-up"),
+    signOut: document.getElementById("sign-out"),
 };
 
 const settingsBtn = {
-    theme: document.getElementById,
-    language: document.getElementById,
+    theme: document.getElementById("theme"),
+    language: document.getElementById("language"),
 };
+
+const searchMovie = document.getElementById("search");

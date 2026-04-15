66fc0a5c72468a7449b516ee4aca69d1df570534
PolaAAshraf <p.ashraf2204@nu.edu.eg>
2026-04-15 17:46:55 +0200

    Basic Navbar

---
+++ src/js/base/navbar.js
@@ -0,0 +1,11 @@
+const accountBtn = {
+    editProfile: document.getElementById,
+    signIn: document.getElementById,
+    signUp: document.getElementById,
+    signOut: document.getElementById,
+};
+
+const settingsBtn = {
+    theme: document.getElementById,
+    language: document.getElementById,
+};

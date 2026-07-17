fd0da6b5f6112151fde6c074868519c726302a34
PolaAAshraf <p.ashraf2204@nu.edu.eg>
2026-07-17 18:43:12 +0300

    add analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import { appProviders as AppProviders } from "@/app/providers";
 import { appRoutes as AppRoutes } from "@/app/routes";
 import Layout from "@/app/Layout";
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App() {
   return (
     <AppProviders>
       <Layout>
         <AppRoutes />
+        <Analytics />
       </Layout>
     </AppProviders>
   );
